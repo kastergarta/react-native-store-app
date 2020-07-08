1cf249320d4ad18b8d57ee6f5cfb62b6fddf169b
Dennis Basyrov <dennis.basyrov@gmail.com>
2020-07-07 23:11:13 -0400

    redux setup + preload data + ProductsOverViewScreen

---
+++ screens/shop/ProductsOverviewScreen.js
@@ -0,0 +1,12 @@
+import React from 'react';
+import { FlatList, StyleSheet } from 'react-native';
+
+const ProductsOverviewScreen = props => {
+
+};
+
+const styles = StyleSheet.create({
+    return <FlatList />;
+});
+
+export default ProductsOverviewScreen;
